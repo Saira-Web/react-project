efd2300d64f9a7482f07f1663c7ac8e57c64f71b
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-07-21 18:32:40 -0400

    First commit

---
+++ src/js/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from 'components/Greeting';
+import 'css/index.css';
+import test from 'img/test.jpg';
+
+const Greeting = ({name}) => {
+	return (
+	<>
+		<h1>Hello, {name}!</h1>
+		<img src={test} alt="Just a test image"/>
+	</>)
+}
+
+export default Greeting;
