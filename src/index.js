081b0c8c6087d5460e400e51127d51edf861a606
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-07-27 13:07:15 -0400

    products updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from 'components/Greeting';
+import footer from 'components/footer';
 import 'css/index.css';
 import test from 'img/test.jpeg';
+import Product from './js/components/product';
 
 const App = () => {
 	return (
-   <Greeting name="World"/>
+   <Product/>
    )
 }
 ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ src/js/components/product.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+const product = [{Name: "laptop", Price:"799"}, {Name: "iPhone", Price:"999"}];
+
+const ProductList = () => {
+    return (
+        <section> 
+            <div>
+                <h2>Products</h2>
+    {product.map ((item) => {return <h1>{item.Name} {item.Price}</h1> })}
+
+            </div>
+        </section>
+    );
+}
+
+export default ProductList;
